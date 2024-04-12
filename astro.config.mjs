b5f24edcc75fc4e3c422a1fbb4b17529ea33148e
playercristian <75957898+playercristian@users.noreply.github.com>
2024-04-12 22:39:35 +0200

    Delete astro.config.mjs

--- astro.config.mjs
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'astro/config';
-import astroI18next from "astro-i18next";
-import tailwind from "@astrojs/tailwind";
-
-export default defineConfig({
-  integrations: [tailwind(), astroI18next()]
-});
